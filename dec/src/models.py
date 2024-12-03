ed3cbd54df9223a56fc6db477e0d7358775334d7
Hamed ajorlou <85940353+hamedajorlou@users.noreply.github.com>
2024-12-03 00:01:24 -0500

    Create models.py

---
+++ dec/src/models.py
@@ -0,0 +1,310 @@
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from torch_geometric.data import DataLoader as GeometricDataLoader
+
+
+import numpy as np
+from copy import deepcopy
+from src.DAGNN import DAGNN, DVAE
+from src.baselines_archs import MLP
+
+# from src.baselines.dagnn import DAGNN
+
+class LinDAGRegModel():
+    def __init__(self, W, Psi):
+        self.W = W
+        self.W_inv = np.linalg.inv(self.W)
+        self.Psi = Psi
+        self.h = np.zeros((Psi.shape[1]))
+
+    def fit(self, X, Y):
+        if isinstance(X, torch.Tensor):
+            X = X.numpy()
+            Y = Y.numpy()
+
+        M = X.shape[0]
+        X_freq = self.W_inv @ X.squeeze().T
+        Zm = np.array([self.W @ np.diag(X_freq[:,m]) @ self.Psi for m in range(M)])
+        ZZ = (Zm.transpose(0, 2, 1) @ Zm).sum(axis=0)        
+        Zy = (Zm.transpose(0, 2, 1) @ Y).sum(axis=0).squeeze()
+
+        if np.linalg.matrix_rank(ZZ) < ZZ.shape[0]:
+            print("WARNING: ZZ is not invertible")
+            self.h = np.linalg.pinv(ZZ) @ Zy
+        else:
+            self.h = np.linalg.inv(ZZ) @ Zy
+
+    def test(self, X, Y):
+        if isinstance(X, torch.Tensor):
+            X = X.numpy()
+            Y = Y.numpy()
+
+        X = X.squeeze().T
+        Y = Y.squeeze().T
+        Y_hat = self.W @ np.diag(self.Psi @ self.h) @ self.W_inv @ X
+        
+        norm_Y = np.linalg.norm(Y, axis=0)
+        norm_Y[norm_Y == 0] = 1
+        err = (np.linalg.norm(Y_hat - Y, axis=0)/norm_Y)**2
+        return err.mean(), err.std()
+
+
+class LinDAGClassModel(LinDAGRegModel):
+    def test(self, X, labels):
+        if isinstance(X, torch.Tensor):
+            X = X.numpy()
+            labels = labels.numpy()
+
+        X = X.squeeze().T
+        labels = labels.squeeze().T
+        Y_hat = self.W @ np.diag(self.Psi @ self.h) @ self.W_inv @ X
+
+        labels_hat = np.argmax(Y_hat, axis=0)
+        return np.sum(labels == labels_hat) / labels.shape[0]
+
+
+class Model():
+    """
+    Model class for learning the weights of a neural network architecture in the context of regression problems.
+
+    This class provides functionality to train and evaluate a neural network model using the provided data.
+
+    Args:
+        arch (torch.nn.Module): Neural network architecture to be trained.
+        loss (torch.nn.modules.loss._Loss, optional): Loss function for training the model. Defaults to torch.nn.CrossEntropyLoss(reduction='sum').
+        device (str or torch.device, optional): Device to run the model on ('cpu' or 'cuda'). Defaults to 'cpu'.
+
+    Attributes:
+        arch (torch.nn.Module): Architecture to be trained.
+        loss_fn (torch.nn.modules.loss._Loss): Loss function for training the model.
+        dev (str or torch.device): Device on which the model is running.
+
+    Methods:
+        _get_loss(X, Y, GSO): Computes the loss for a given input-output pair.
+        fit(X, Y, lr, n_epochs, bs=100, wd=0, optim=torch.optim.Adam, eval_freq=10, patience=100, verb=False): Trains the model using the provided data.
+
+    """
+    def __init__(self, arch, loss=torch.nn.MSELoss(reduction='mean'), device='cpu'):
+        self.arch = arch.to(device)
+        self.loss_fn = loss
+        self.dev = device
+        self.MLP_arch = isinstance(arch, MLP)
+        self.DAGNN_arch = isinstance(arch, DAGNN)
+        self.DVAE_arch = isinstance(arch, DVAE)
+
+
+    def _get_loss(self, X, Y, GSO):
+        self.arch.eval()
+        with torch.no_grad():
+            if self.DAGNN_arch:
+                Y = torch.stack([data.y.to(self.dev) for data in X])
+                X = self.arch._collate_fn(X)
+                Y_hat = self.arch.encode(X)
+            elif self.DVAE_arch:
+                Y = torch.stack([data['y'].to(self.dev) for data in X])
+                X = self.arch._collate_fn(X)
+                Y_hat = self.arch(X)
+            else:
+                Y_hat = self.arch(X) if self.MLP_arch else self.arch(X, GSO)
+            return self.loss_fn(Y_hat, Y).item()
+        
+
+
+    def _init_optimizer(self, optim, lr, wd):
+        self.opt = optim(self.arch.parameters(), lr=lr, weight_decay=wd)
+
+    def _train_batches(self, train_dl, GSO, iters, optimizer):
+        """
+        Perform multiple training iterations on batches of data.
+        """
+        if iters < 1:
+            return None
+
+        for _ in range(iters):
+            if self.DAGNN_arch:
+                for Xb in train_dl:
+                    Yb = torch.stack([data.y.to(self.dev) for data in Xb])
+                    Xb = self.arch._collate_fn(Xb)
+                    Y_hat = self.arch.encode(Xb)
+                    loss_train = self.loss_fn(Y_hat, Yb)
+                    loss_train.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+
+            elif self.DVAE_arch:
+                for Xb in train_dl:
+                    Yb = torch.stack([data['y'].to(self.dev) for data in Xb])
+                    Xb = self.arch._collate_fn(Xb)
+                    Y_hat = self.arch(Xb)
+                    loss_train = self.loss_fn(Y_hat, Yb)
+                    loss_train.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+            else:
+                for Xb, Yb in train_dl:
+                    if self.MLP_arch:
+                        Y_hat = self.arch(Xb)
+                    else:
+                        Y_hat = self.arch(Xb, GSO)
+                    loss_train = self.loss_fn(Y_hat, Yb)
+
+                    loss_train.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+        return loss_train.item()
+
+    def _train_epoch(self, train_dl, GSO):
+        return self._train_batches(train_dl, GSO, 1, self.opt)
+              
+    def fit(self, X, Y, GSO, lr, n_epochs, bs=100, wd=0, optim=torch.optim.Adam, eval_freq=10,
+              patience=100, verb=False, track_test_err=False):
+        """
+        Trains the model using the provided data.
+
+        Args:
+            X (dict): Dictionary containing input data. It should contain keys 'train', 'val', and optionally 'test'.
+            Y (dict): Dictionary containing target data corresponding to input data. It should contain keys 'train', 'val', and optionally 'test'.
+            lr (float): Learning rate for optimization.
+            n_epochs (int): Number of epochs for training.
+            bs (int, optional): Batch size for training. Defaults to 100.
+            wd (float, optional): Weight decay (L2 regularization) parameter. Defaults to 0.
+            optim (torch.optim.Optimizer, optional): PyTorch optimizer class. Defaults to torch.optim.Adam.
+            eval_freq (int, optional): Frequency of evaluation (in epochs). Defaults to 20.
+            patience (int, optional): Number of epochs to wait for improvement in validation loss before early stopping. Defaults to 100.
+            verb (bool, optional): Verbosity flag to print progress. Defaults to False.
+
+        Returns:
+            dict: Dictionary containing losses for training, validation, and test data (if available).
+        """
+        # Checks input
+        assert X.keys() == Y.keys(), 'X and Y contain different keys' 
+        assert 'val' in X, 'Missing validation data'
+
+        # Extract data
+        GSO = GSO.to(self.dev) if not self.MLP_arch else None
+        if self.DAGNN_arch :
+            pyg_data_train = [data.to(self.dev) for data in X['train']]
+            X_val = [data1.to(self.dev) for data1 in X['val']]
+            X_test = [data2.to(self.dev) for data2 in X['test']]
+            Y_val = Y['val'].to(self.dev)
+            Y_test = Y['test'].to(self.dev)
+            train_dl = [pyg_data_train[i:i+bs] for i in range(0, len(pyg_data_train), bs)]
+        elif self.DVAE_arch :
+            X_val = X['val']
+            X_test = X['test']
+            Y_val = Y['val'].to(self.dev)
+            Y_test = Y['test'].to(self.dev)
+            train_dl = [X['train'][i:i+bs] for i in range(0, len(X['train']), bs)]
+        else:
+            train_ds = TensorDataset(X['train'].to(self.dev), Y['train'].to(self.dev))
+            train_dl = DataLoader(train_ds, batch_size=bs)
+            X_val, Y_val = X['val'].to(self.dev), Y['val'].to(self.dev)
+            X_test, Y_test = X['test'].to(self.dev), Y['test'].to(self.dev) if 'test' in X else None
+        # Initialize variables
+        best_val_loss = float('inf')
+        cont_stop = 0
+        best_weights = deepcopy(self.arch.state_dict())
+        losses_train, losses_val, losses_test = [np.zeros(n_epochs) for _ in range(3)]
+
+        self._init_optimizer(optim, lr, wd)
+        
+        # TODO: Try an scheduler?
+
+        # Training loop
+        for epoch in range(n_epochs):            
+            self.arch.train()
+            losses_train[epoch] = self._train_epoch(train_dl, GSO)
+            losses_val[epoch] = self._get_loss(X_val, Y_val, GSO)
+            if track_test_err and X_test is not None:
+                losses_test[epoch] = self._get_loss(X_test, Y_test, GSO)
+
+            if (epoch == 0 or (epoch+1) % eval_freq == 0) and verb:
+                print(f"Epoch {epoch+1}/{n_epochs} - Loss Train: {losses_train[epoch]:.6f} - Val Loss: {losses_val[epoch]:.6f}", flush=True)
+
+            # TODO: this is not going to work well for src id!
+            # Early stopping based on validation loss
+            if losses_val[epoch] < best_val_loss:
+                best_val_loss = losses_val[epoch]
+                best_weights = deepcopy(self.arch.state_dict())
+                cont_stop = 0
+            else:
+                cont_stop += 1
+
+            if cont_stop >= patience:
+                if verb:
+                    print(f'\tConvergence achieved at iteration: {epoch}')
+                break
+
+        self.arch.load_state_dict(best_weights)
+        return {'train': losses_train, 'val': losses_val, 'test': losses_test}
+
+    def test(self, X, Y, GSO, arc):
+        if arc == DAGNN:
+            X = [data.to(self.dev) for data in X]
+            Y = Y.to(self.dev)
+            X = self.arch._collate_fn(X)
+            Y_hat = self.arch.encode(X)
+        elif arc == DVAE :
+            Y = Y.to(self.dev)
+            X = self.arch._collate_fn(X)
+            Y_hat = self.arch(X)
+        else:
+            X = X.to(self.dev)
+            Y = Y.to(self.dev)
+            GSO = GSO.to(self.dev) if not self.MLP_arch else None
+            Y_hat = self.arch(X) if self.MLP_arch else self.arch(X, GSO)
+        Y_hat = Y_hat.cpu().detach().numpy().squeeze().T
+        Y = Y.cpu().detach().numpy().squeeze().T
+        norm_Y = np.linalg.norm(Y, axis=0)
+        norm_Y[norm_Y == 0] = 1
+        err = (np.linalg.norm(Y_hat - Y, axis=0)/norm_Y)**2
+        return err.mean(), err.std()
+
+
+class SrcIdModel(Model):
+    def __init__(self, arch, loss=torch.nn.CrossEntropyLoss(reduction='mean'), device='cpu'):
+        super(SrcIdModel, self).__init__(arch, loss, device)
+    
+    def test(self, X, labels, GSO=None):
+        X = X.to(self.dev)
+        labels = labels.to(self.dev)
+        GSO = GSO.to(self.dev) if not self.MLP_arch else None
+
+        # Get list of true source nodes
+        labels = labels.cpu().detach().numpy().squeeze()
+
+        # Get estimated labels
+        Y_hat = self.arch(X) if self.MLP_arch else self.arch(X, GSO)
+        Y_hat = Y_hat.cpu().detach().numpy().squeeze()
+        labels_hat = np.argmax(Y_hat, axis=1)
+        
+        return np.sum(labels == labels_hat)/labels.shape[0]
+
+
+class AlternatingModel(Model):
+    def __init__(self, arch, loss=torch.nn.MSELoss(reduction='mean'), epochs_h=1,
+                 epochs_W=1, device='cpu'):
+        super().__init__(arch, loss, device)
+        self.epochs_h = epochs_h
+        self.epochs_W = epochs_W
+        
+    def _init_optimizer(self, optim, lr, wd):
+        W_params = [layer.W for layer in self.arch.convs]
+        if self.arch.bias:
+            W_params += [layer.b for layer in self.arch.convs]
+
+        h_params = [layer.h for layer in self.arch.convs]
+
+        self.opt_h = optim(h_params, lr=lr, weight_decay=wd)
+        self.opt_W = optim(W_params, lr=lr, weight_decay=wd)
+
+    def _train_epoch(self, train_dl, GSO):
+        # Optimization over h
+        self._train_batches(train_dl, GSO, self.epochs_h, self.opt_h)
+        # Optimization over W
+        return self._train_batches(train_dl, GSO, self.epochs_W, self.opt_W)
+        
+        
